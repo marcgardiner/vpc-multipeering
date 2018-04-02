77f0bea19a33a4bb5e4c3dbb304c6f60e557bbcf
marcgardiner <marcgardinerdriven@gmail.com>
2018-04-02 18:12:56 +1000

    active create_vpc function

--- vpc-create.py
+++ vpc-create.py
@@ -79,7 +79,7 @@ def main():
     conn = boto3.resource('ec2', aws_access_key_id=AWS_ACCESS_KEY_ID,
                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                           region_name=REGION_NAME)
-    # create_vpc(conn)
+    create_vpc(conn)
 
 
 def create_vpc(conn):
